f5bb738280d0dea9c23cdeace13b798ffac61f1f
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2024-05-02 19:06:53 -0400

    Update urls.py

--- pdfapp/urls.py
+++ pdfapp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user-details/', views.user_details_form, name='user_details_form'),
+    path('', views.user_details_form, name='user_details_form'),
+    # path('user-details/', views.user_details_form, name='user_details_form'),
     path('direct-deposit/', views.direct_deposit_form, name='direct_deposit_form'),
     path('contractor-agreement/', views.contractor_agreement_form, name='contractor_agreement_form'),
     path('commission-agreement/', views.commission_agreement_form, name='commission_agreement_form'),
