3a2a57e87f2775a55426613c1cafe195d1027cdf
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2024-05-02 19:24:13 -0400

    added allowed host

--- pdf_project/settings.py
+++ pdf_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zm1j4=^x!(ev45o&pj*#c%xic144btoxr684c*($m4-!nmc-ms
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
