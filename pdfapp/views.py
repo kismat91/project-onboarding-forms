a847f8eb112ddecc2aa4db59a029a7178bf802aa
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2024-05-09 13:37:43 -0400

    Update views.py

--- pdfapp/views.py
+++ pdfapp/views.py
@@ -102,7 +102,7 @@ def user_details(request):
             db.add_record(web_form_fields['first_name'], web_form_fields['email'], web_form_fields['home_phone'], web_form_fields['street_address'], web_form_fields['ssn_or_gov_id'], OUTPUT_LOCAL_FOLDER_PATH)
             email_sender.connect()
             recipient = 'kismatkhatri91@gmail.com'
-            email_sender.send_email(recipient, f'Form Submitted by {web_form_fields["first_name"]}', """For was submitted you can check at http://127.0.0.1:8000/log_in/""")
+            email_sender.send_email(recipient, f'Form Submitted by {web_form_fields["first_name"]}', f"""For was submitted you can check at {request.build_absolute_uri('/log_in/')}""")
             email_sender.close_connection()
 
             return redirect('success')
