32be08ff5ea9e485439aa788418fc49a3c7a3a11
Giri Raj Koirala <103231513+KoiralaGiri@users.noreply.github.com>
2024-05-11 10:49:22 -0400

    updated

--- pdfapp/views.py
+++ pdfapp/views.py
@@ -22,7 +22,7 @@ def process_pdf(pdf_template_path, output_path, data_dict):
 
 def user_details(request):
     print(session_id)
-    db = DatabaseManager("/root/project/real_estate_onboarding.db)
+    db = DatabaseManager("/root/project/real_estate_onboarding.db")
     email_sender = EmailSender()
     if request.method == 'POST':
         form = CombinedForm(request.POST)
