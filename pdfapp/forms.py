64b65ac9065b3eb534e297fb8daa3f58b3355221
kismat91 <99793019+kismat91@users.noreply.github.com>
2024-06-20 17:25:59 -0400

    date update

--- pdfapp/forms.py
+++ pdfapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import UserDetails, ContractorAgreement, CommissionAgreement
+import datetime
 
 class CombinedForm(forms.ModelForm):
     # Additional fields from DirectDeposit
